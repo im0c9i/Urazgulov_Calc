fee4b66eb2fd6b1d12d390aa66d1bc4f741e1b74
Ramil <ramil-urazgulov444@mail.ru>
2023-03-29 13:25:49 +0700

    Calc

---
+++ src/com/company/service/DivideService.java
@@ -0,0 +1,13 @@
+package com.company.service;
+
+import com.company.exception.DividingByZeroException;
+
+public class DivideService implements CalculateService{
+    @Override
+    public double calculate(double a, double b) throws DividingByZeroException {
+        if(b == 0) {
+            throw new DividingByZeroException("Dividing by zero is forbidden!");
+        }
+        return a / b;
+    }
+}
